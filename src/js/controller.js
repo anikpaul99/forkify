b8d43770845aa42ea599abccf2a0a93d25ef7b7b
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-06-21 08:48:03 +0600

    :sparkles: feat: add display recipe

---
+++ src/js/controller.js
@@ -0,0 +1,45 @@
+import * as Model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+/**
+ * Will control loading and rendering recipe data.
+ * @returns {undefined}
+ * @author Anik Paul
+ */
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await Model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(Model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
